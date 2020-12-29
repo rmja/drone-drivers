61fd50dc153f1f04a1d0f983ae96563afd6329b3
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2020-12-29 15:28:09 +0100

    Add at25010b example app

---
+++ examples/at25010b/src/tasks/root.rs
@@ -0,0 +1,131 @@
+//! The root task.
+
+use core::convert::TryInto;
+
+use crate::{consts, thr, thr::ThrsInit, Regs};
+use drone_at250x0_drv::{At250x0Drv, At250x0Kind};
+use drone_core::log;
+use drone_cortexm::{drv::sys_tick::SysTick, reg::prelude::*, swo, thr::prelude::*};
+use drone_stm32_map::periph::{
+    dma::{periph_dma2, periph_dma2_ch2, periph_dma2_ch3},
+    gpio::{
+        periph_gpio_a5, periph_gpio_a6, periph_gpio_a7, periph_gpio_a_head, periph_gpio_i1,
+        periph_gpio_i_head,
+    },
+    spi::periph_spi1,
+    sys_tick::periph_sys_tick,
+};
+use drone_stm32f4_hal::{
+    dma::{config::*, DmaCfg},
+    gpio::{prelude::*, GpioHead},
+    rcc::{periph_flash, periph_pwr, periph_rcc, traits::*, Flash, Pwr, Rcc, RccSetup},
+    spi::{chipctrl::*, config::*, prelude::*, SpiDrv},
+};
+
+/// The root task handler.
+#[inline(never)]
+pub fn handler(reg: Regs, thr_init: ThrsInit) {
+    let thr = thr::init(thr_init);
+
+    thr.hard_fault.add_once(|| panic!("Hard Fault"));
+
+    println!("Hello, world!");
+
+    // Enable interrupts.
+    thr.rcc.enable_int();
+    thr.spi_1.enable_int();
+    thr.dma_2_ch_2.enable_int();
+    thr.dma_2_ch_3.enable_int();
+
+    // Initialize clocks.
+    let rcc = Rcc::init(RccSetup::new(periph_rcc!(reg), thr.rcc));
+    let pwr = Pwr::init(periph_pwr!(reg));
+    let flash = Flash::init(periph_flash!(reg));
+
+    let hseclk = rcc.stabilize(consts::HSECLK).root_wait();
+    let pll = rcc
+        .select(consts::PLLSRC_HSECLK, hseclk)
+        .stabilize(consts::PLL)
+        .root_wait();
+    let hclk = rcc.configure(consts::HCLK);
+    let pclk1 = rcc.configure(consts::PCLK1);
+    let pclk2 = rcc.configure(consts::PCLK2);
+    pwr.enable_overdrive();
+    flash.set_latency(consts::HCLK.get_wait_states(VoltageRange::HighVoltage));
+    swo::flush();
+    swo::update_prescaler(consts::HCLK.f() / log::baud_rate!() - 1);
+    rcc.select(consts::SYSCLK_PLL, pll.p());
+
+    // Enable IO port clock.
+    let gpio_a = GpioHead::with_enabled_clock(periph_gpio_a_head!(reg));
+    let gpio_i = GpioHead::with_enabled_clock(periph_gpio_i_head!(reg));
+
+    // Configure SPI GPIO pins.
+    let pin_sck = gpio_a
+        .pin(periph_gpio_a5!(reg))
+        .into_af()
+        .into_pp()
+        .into_nopull()
+        .with_speed(GpioPinSpeed::HighSpeed);
+    let pin_miso = gpio_a
+        .pin(periph_gpio_a6!(reg))
+        .into_af()
+        .into_pp()
+        .into_nopull()
+        .with_speed(GpioPinSpeed::HighSpeed);
+    let pin_mosi = gpio_a
+        .pin(periph_gpio_a7!(reg))
+        .into_af()
+        .into_pp()
+        .into_nopull()
+        .with_speed(GpioPinSpeed::HighSpeed);
+    let pin_cs = gpio_i
+        .pin(periph_gpio_i1!(reg))
+        .into_output()
+        .into_pp()
+        .into_pullup()
+        .with_speed(GpioPinSpeed::HighSpeed);
+
+    // Initialize dma.
+    let dma2 = DmaCfg::with_enabled_clock(periph_dma2!(reg));
+    let miso_dma = dma2.ch(DmaChSetup::new(periph_dma2_ch2!(reg), thr.dma_2_ch_2));
+    let mosi_dma = dma2.ch(DmaChSetup::new(periph_dma2_ch3!(reg), thr.dma_2_ch_3));
+
+    // Initialize spi.
+    let pins = SpiPins::default()
+        .sck(&pin_sck)
+        .miso(&pin_miso)
+        .mosi(&pin_mosi);
+    let setup = SpiSetup::new(
+        periph_spi1!(reg),
+        thr.spi_1,
+        pins,
+        pclk2,
+        BaudRate::Max(5_000_000),
+    );
+    let spi_drv = SpiDrv::init(setup);
+    let mut spi = spi_drv.init_master(miso_dma, mosi_dma);
+
+    let mut chip = SpiChip::init(pin_cs);
+
+    let timer = SysTick::new(periph_sys_tick!(reg), thr.sys_tick);
+    let eeprom = At250x0Drv::new(At250x0Kind::At25010b, timer);
+
+    const TMIB_2030: [u8; 48] = [
+        0x74, 0x6D, 0x02, 0x63, 0x72, 0x6D, 0x33, 0x30, 0x39, 0x35, 0x30, 0x62, 0x2D, 0x31, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0xEE, 0x07, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0xFB, 0x11, 0x00, 0x00, 0x99,
+        0x00, 0x98, 0x9C,
+    ];
+
+    eeprom
+        .write(&mut spi, &mut chip, 0x00, &TMIB_2030)
+        .root_wait()
+        .unwrap();
+
+    let mut buf = [0u8; 48];
+    eeprom.read(&mut spi, &mut chip, 0x00, &mut buf).root_wait();
+
+    // Enter a sleep state on ISR exit.
+    reg.scb_scr.sleeponexit.set_bit();
+}
---
+++ examples/at25010b/src/thr.rs
@@ -0,0 +1,38 @@
+//! The threads.
+
+pub use drone_cortexm::thr::{init, init_extended};
+pub use drone_stm32_map::thr::*;
+
+use drone_cortexm::thr;
+
+thr! {
+    /// The thread data.
+    thread => pub Thr {};
+
+    /// The thread-local storage.
+    local => pub ThrLocal {};
+
+    /// The vector table type.
+    vtable => pub Vtable;
+
+    /// A set of thread tokens.
+    index => pub Thrs;
+
+    /// Threads initialization token.
+    init => pub ThrsInit;
+
+    threads => {
+        exceptions => {
+            /// All classes of faults.
+            pub hard_fault;
+            pub sys_tick;
+        };
+        interrupts => {
+            // Vector table for stm32f429 is in PM0090 table 62 page 375.
+            5: pub rcc;
+            35: pub spi1;
+            58: pub dma2_ch2; // SPI1_RX: DMA2, stream 2 (channel 3).
+            59: pub dma2_ch3; // SPI1_TX: DMA2, stream 3 (channel 3).
+        };
+    };
+}
